a5d6c137c138fb5fc5b0dda7d77c06a44f557b1c
Hwhahajaja <150585398+Hwhahajaja@users.noreply.github.com>
2023-12-07 13:29:39 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import keep_alive
+import keep_alive import flask
 import discord, asyncio, random, os, requests, sys, threading, datetime, json, aiohttp
 from urllib import parse
 import re, time
